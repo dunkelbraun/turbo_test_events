28587c3b0a4cbd78fa55068a820e4ee999c5626d
Marcos Essindi <marcessindi@icloud.com>
2020-08-07 10:43:14 +0200

    Set @observer_peers to Concurrent::Map

--- lib/turbo_test_events/event.rb
+++ lib/turbo_test_events/event.rb
@@ -1,35 +1,28 @@
 # frozen_string_literal: true
 
 require "observer"
+require "concurrent"
 
 module TurboTest
   class Event
     include Observable
+
+    def initialize
+      @observer_peers = Concurrent::Map.new
+    end
 
     def publish(*arg)
       changed
       notify_observers(*arg.map(&:freeze))
     end
 
-    alias subscribe_without_locking add_observer
+    alias subscribe add_observer
     undef :add_observer
 
-    def subscribe(subscriber, func=:update)
-      Mutex.new.synchronize { subscribe_without_locking(subscriber, func) }
-    end
-
-    alias unsubscribe_without_locking delete_observer
+    alias unsubscribe delete_observer
     undef :delete_observer
 
-    def unsubscribe(subscriber)
-      Mutex.new.synchronize { unsubscribe_without_locking(subscriber) }
-    end
-
-    alias unsubscribe_all_without_locking delete_observers
+    alias unsubscribe_all delete_observers
     undef :delete_observers
-
-    def unsubscribe_all
-      Mutex.new.synchronize { unsubscribe_all_without_locking }
-    end
   end
 end
